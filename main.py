91f9d364192365872a54efcfa2556e5a30f7b274
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2023-11-19 14:13:11 +0100

    :)

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
+# Importing the 'Rectangle' class from the file 'rectangle1'
 from rectangle1 import Rectangle
 
+# Asking the user to input the length and width of a rectangle
 l = int(input("Enter the length of the rectangle: "))
 w = int(input("Enter the width of the rectangle: "))
 
+# Creating an instance of the 'Rectangle' class using user-provided dimensions
 rectangle1 = Rectangle(l, w)   
 
 print("\nRectangle Properties : ")
 
-rectangle1.Perimeter()
-
-rectangle1.Area()
-
-rectangle1.isSquare()
-
-rectangle1.DisplayRectangle()
+# Displaying the properties of the rectangle using the 'DisplayRectangle' method
+rectangle1.DisplayRectangle()  
